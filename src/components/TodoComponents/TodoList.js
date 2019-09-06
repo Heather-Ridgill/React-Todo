04f33928de2a12a158e014873542953d7e95e8eb
Heather-Ridgill <Heather.Ridgill@gmail.com>
2019-09-05 18:23:28 -0700

    Set-App-Structure

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,31 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+import Todo from "./Todo.js";
+
+const TodoList = props => {
+  return (
+    <div className="My-List">
+      {props.list.map(item => (
+        <Todo key={item.id} item={item} toggleItem={props.toggleItem} />
+      ))}
+      <button className="clear-btn" onClick={props.clearPurchased}>
+        Clear Purchased
+      </button>
+    </div>
+  );
+};
+
+//     if(!todos.legnth) {
+//         return(
+//              <h4> Nothing to do.... yet! </h4>
+//     );
+// } else {
+//     return(
+//         <div>
+//             {todos.map(todo => {
+//                 return <Todo key={todo.id} todo={todo} />
+//             })}
+//         </div>
+//     )
+// }
+
+export default TodoList;
